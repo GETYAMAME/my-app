2329850b38a74cfec706842afb5c09eddb0a9146
GETYAMAME <pon.mikan10@gmail.com>
2021-08-17 21:05:18 +0900

    GraphQLと疎通

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Link from 'next/link'
 import { useEffect,useState } from 'react'
 import styled from 'styled-components'
 import { Header } from '../components/header.js'
+import gql from "graphql-tag";
+import { ApolloClient, InMemoryCache, HttpLink } from "apollo-boost";
+import { Query, ApolloProvider } from "react-apollo";
 
 const Container = styled.div`
   padding:10px 20px;
@@ -16,6 +19,48 @@ const MessageItem = styled.div`
   border-top: solid 3px #5d627b;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.22);
 `
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://quotations-td8.herokuapp.com/v1/graphql",
+  }),
+});
+
+const MY_QUERY_QUERY = gql`
+  query MyQuery {
+    Quotations {
+      id
+      message
+      speaker
+      url
+    }
+  }
+`;
+
+const MyQueryQuery = (props) => {
+  return (
+    <Query
+      query={MY_QUERY_QUERY} >
+      {({ loading, error, data }) => {
+        if (loading) return <pre>Loading</pre>
+        if (error)
+          return (
+            <pre>
+              Error in MY_QUERY_QUERY
+              {JSON.stringify(error, null, 2)}
+            </pre>
+          );
+    
+        if (data) {
+          return (
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          )
+        }
+      }}
+    </Query>
+  )
+};
+
 
 export default function Home() {
   const ITEMS = [
@@ -28,6 +73,7 @@ export default function Home() {
     //setItem(ITEMS);
   },[])
   return (
+    <ApolloProvider client={apolloClient}>
     <Container>
       <Header tab={'top'}></Header>
       <h2>テスト</h2>
@@ -36,6 +82,8 @@ export default function Home() {
           return <MessageItem key={index}>{str[0]}：{str[1]}</MessageItem>
         })
       }
-    </Container>    
+    </Container>
+    <MyQueryQuery  />
+   </ApolloProvider> 
   )
 }
